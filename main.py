f9bc0b0cf69eb8a385fc41765571135e82756e95
Skryptor <20Dimon19997@gmail.com>
2024-11-09 22:19:58 +0300

    SLOJNO_NO_CLASSNO

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ class Student:
         self.name = name
         self.surname = surname
         self.gender = gender
-        self.finished_courses = []
+        self.finished_courses = ['Введение в программирование']
         self.courses_in_progress = []
         self.grades = {}
 
@@ -16,6 +16,23 @@ class Student:
         else:
             return 'Ошибка'
 
+    def calculate_grades(self):
+        all_grades = 0
+        i = 0
+        for grades in self.grades.values():
+            all_grades += sum(grades)
+            i += len(grades)
+        Averaga = all_grades / i
+        return round(Averaga, 1)
+    
+    def __str__(self):
+        return (F'Студенты \n'
+                F'Имя: {self.name}'
+                F'\nФамилия: {self.surname}'
+                F'\nСредняя оценка за домашние задания: {self.calculate_grades()} '
+                F'\nКурсы в процессе изучения: {' '.join(self.courses_in_progress)} '
+                F'\nЗавершенные курсы: {' '.join(self.finished_courses)}')
+
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
@@ -27,6 +44,19 @@ class Lecturer(Mentor):
     def __init__(self,name, surname):
         super().__init__(name,surname)
         self.grades_Fstudent = {}
+
+    def calculate_grades(self):
+        all_grades = 0
+        i = 0
+        for grades in self.grades_Fstudent.values():
+            all_grades += sum(grades)
+            i += len(grades)
+        Averaga = all_grades / i
+        return round(Averaga, 1)
+
+    def __str__(self):
+        return (F'Лекторы \nИмя: {self.name}\nФамилия: {self.surname} '
+                F'\nСредняя оценка за лекции: {self.calculate_grades()} ')
 
 class Reviewer(Mentor):
     def __init__(self, name, surname):
@@ -42,24 +72,30 @@ class Reviewer(Mentor):
         else:
             return 'Ошибка'
 
+    def __str__(self):
+        return F'Эксперты \nИмя: {self.name}\nФамилия: {self.surname}'
+
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
 
-cool_mentor = Reviewer('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
+cool_reviewer = Reviewer('Some', 'Buddy')
+cool_reviewer.courses_attached += ['Python']
 
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 9)
+cool_reviewer.rate_hw(best_student, 'Python', 7)
+cool_reviewer.rate_hw(best_student, 'Python', 10)
 
 cool_lecturer = Lecturer('Ivan', 'Petrov')
 cool_lecturer.courses_attached.append('Python')
 
+best_student.rate_lecturer(cool_lecturer, 'Python', 9)
 best_student.rate_lecturer(cool_lecturer, 'Python', 10)
-best_student.rate_lecturer(cool_lecturer, 'Python', 10)
-best_student.rate_lecturer(cool_lecturer, 'Python', 10)
+best_student.rate_lecturer(cool_lecturer, 'Python', 9)
 
 
 
-print(best_student.grades)
-print(cool_lecturer.grades_Fstudent)
+print(cool_reviewer)
+
+print(cool_lecturer)
+
+print(best_student)
